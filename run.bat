2240344252962031d76582e570e09aa96fc4d4d3
Atomvapen <GustafWik@protonmail.com>
2024-12-18 18:58:21 +0100

    Refactor build configuration and update main application logic
    - Renamed the project from "Zonk" to "Zink" in build.zig and build.zig.zon.
    - Updated the main source file reference in the build configuration.
    - Removed the old root.zig file and its associated tests.
    - Introduced a new Buffer struct in main.zig for handling input lines.
    - Implemented line counting and formatted output in the main function.
    - Removed obsolete test cases and streamlined the codebase.

---
+++ run.bat
@@ -0,0 +1,1 @@
+start cmd.exe /k "zig build run -- src/main.zig"
--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,86 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
+const max_height = 24;
+
+pub const BufferError = error{OutofMemory};
+
+pub const Buffer = struct {
+    allocator: *std.mem.Allocator,
+    content: []u8,
+    line_it: std.mem.SplitIterator(u8, .any),
+
+    pub fn init(allocator: *std.mem.Allocator, content: []const u8) !Buffer {
+        return Buffer{
+            .allocator = allocator,
+            .content = try allocator.dupe(u8, content),
+            .line_it = std.mem.splitAny(u8, content, "\n"),
+        };
+    }
+
+    pub fn nextLine(self: *Buffer) ?[]const u8 {
+        return self.line_it.next();
+    }
+
+    pub fn lineReset(self: *Buffer) void {
+        self.line_it.index = 0;
+    }
+};
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var allocator = arena.allocator();
+    const content = try readInput(&allocator);
+    var buffer = try Buffer.init(&allocator, content);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var line_count: u32 = 1;
+    while (buffer.nextLine()) |line| : (line_count += 1) {
+        if (line_count == max_height) break;
+        _ = line;
+    }
+    const max_line_count_width: u32 = numberWidth(line_count);
+    buffer.lineReset();
 
-    try bw.flush(); // Don't forget to flush!
+    line_count = 1;
+    while (buffer.nextLine()) |line| : (line_count += 1) {
+        if (line_count == max_height) break;
+        {
+            var i = max_line_count_width - numberWidth(line_count);
+            while (i != 0) : (i -= 1) {
+                std.debug.print(" ", .{});
+            }
+        }
+        std.debug.print("{d} {s}\n", .{ line_count, line });
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn numberWidth(number: u32) u32 {
+    var result: u32 = 0;
+    var n = number;
+
+    while (n != 0) : (n /= 10) {
+        result += 1;
+    }
+    return result;
 }
 
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+fn readInput(allocator: *std.mem.Allocator) ![]u8 {
+    var arg_it = try std.process.argsWithAllocator(allocator.*);
+    defer arg_it.deinit();
+    // _ = arg_it.next() orelse unreachable;
+    _ = arg_it.skip();
+
+    const file_name = arg_it.next();
+    var file_handle = blk: {
+        if (file_name) |file_name_delimited| {
+            const fName: []const u8 = file_name_delimited;
+            break :blk try std.fs.cwd().openFile(fName, .{});
+        } else {
+            break :blk std.io.getStdIn();
         }
     };
-    try std.testing.fuzz(global.testOne, .{});
+
+    defer file_handle.close();
+    return try file_handle.readToEndAlloc(allocator.*, std.math.maxInt(usize));
 }
